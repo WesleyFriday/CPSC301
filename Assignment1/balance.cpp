ec7ed7c3d29173967e17217d6dc142b2c8b3714d
Wesley Friday <43228841+WesleyFriday@users.noreply.github.com>
2019-04-16 23:17:06 -0700

    Add files via upload

--- Assignment1/balance.cpp
+++ Assignment1/balance.cpp
@@ -14,114 +14,125 @@ struct  PERSON {
   };
 
 //Function Protypes
-void Display(PERSON arr[], int count);
-void FindtheRiches(PERSON arr[], int count);
-void Deposit(string name, PERSON arr[], int count);
+void Display(PERSON *p, int count);
+void FindtheRiches(PERSON *p, int count);
+void Deposit(string name, PERSON *p, int count);
 PERSON *readData(int &N);
-oid NewCopy(PERSON arr[], int count);
+void NewCopy(PERSON* p, int count);
 
 //Main
 int main() {
+
     fstream filein;
-    ofstream fileout;
     filein.open("data.txt");
-    string fname, lname, n, user;
-    float balance;
-    int count = 0;
+    int count = 6;
+    string user;
 
-
-    PERSON *people;
-    people = new PERSON[count];
-
-    string u;
-    while(getline(filein, u) )
-    {
-      count++;
-    };
-
-
+//Getting the Count
+    // while(!filein.eof() )
+    // {
+    //   string u;
+    //   getline(filein, u);
+    //   count++;
+    // };
 filein.close();
 
-filein.open("data.txt");
-    for (int i = 0; i < count; i++)
-	{
-    filein >> fname;
-    filein >> lname;
-    filein >> balance;
-    filein >> ws;
-    n = fname + " " + lname;
 
-    strcpy(people[i].Name, n.c_str());
-    people[i].Balance = balance;
+PERSON* per = new PERSON[count];
+per = readData(count);
 
-  };
 
-  Display(people, count);
-  FindtheRiches(people, count);
 
+
+  Display(per,count);
+  FindtheRiches(per, count);
+  //User Input
   cout << "Please insert name:  ";
   getline(cin, user);
-  Deposit(user, people, count);
-    filein.close();
+  Deposit(user,per, count);
+  //NewCopy(per, count);
+
     return 0;
 }
 
 //Display the Array
-void Display(PERSON arr[], int count)
+void Display(PERSON* p, int count)
 {
   cout << "Name       Balance" << endl << "--------------"<< endl;
 
   for(int i = 0; i < count; i++)
   {
-    cout << arr[i].Name << "   $" << arr[i].Balance << endl;
+    cout << p[i].Name << "       $" << p[i].Balance << endl;
   }
 
 };
 
 //Find the Richest
-void FindtheRiches(PERSON arr[], int count)
+void FindtheRiches(PERSON * p, int count)
 {
 float richest = 0;
 string richperson;
 
   for(int i = 0; i < count; i++)
   {
-    if(arr[i].Balance < arr[i + 1].Balance)
-    { richest = arr[i + 1].Balance; richperson = arr[i + 1].Name; }
+    if(p[i].Balance < p[i + 1].Balance)
+    { richest = p[i + 1].Balance; richperson = p[i + 1].Name;}
   };
 cout << "The richest person is " << richperson << endl << endl;
 };
 
 //Deposit Function
-void Deposit(string name, PERSON arr[], int count)
+void Deposit(string name,PERSON * p, int count)
 {
   float bal = 0.0;
   int num;
 
   for(int i = 0; i < count; i++)
   {
-    strcmp(name.c_str(), arr[i].Name);
-    if (name == arr[i].Name){num = i;};
+    strcmp(name.c_str(), p[i].Name);
+    if (name == p[i].Name){num = i;};
   };
 
-    if(name == arr[num].Name)
+    if(name == p[num].Name)
     {
       cout << "Enter how much you want to deposit?: ";
       cin >> bal;
-      arr[num].Balance += bal;
-      cout << arr[num].Name << " your new balance is " << arr[num].Balance << endl;
+      p[num].Balance += bal;
+      cout << p[num].Name << " your new balance is " << p[num].Balance << endl;
     }
 
 };
 
-void NewCopy(PERSON arr[], int count)
+// void NewCopy(PERSON * p, int count)
+// {
+//   fstream out;
+//   out.open("data.txt");
+//   out << p->Name << p[count].Balance << endl;
+//   out.close();
+// };
+
+
+
+PERSON *readData(int &N)
 {
-  fstream out;
-  out.open("data.txt")
-  out << arr[i].name << arr[i].balance << endl;
-  out.close();
+PERSON* person = new PERSON[N];
+ifstream filein;
+filein.open("data.txt");
+string fname, lname;
+string fulName;
+int t = 0;
+
+
+while (filein && t < N)
+{
+filein >> fname >> lname >> person[t].Balance;
+fulName = fname + " " + lname;
+strcpy(person->Name, fulName.c_str());
+cout << fname << person->Balance << endl;
+t++;
+
 }
 
-
-
-PERSON *readData(int &N){};
+  filein.close();
+  return person;
+};
